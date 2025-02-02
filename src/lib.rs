5bb735fa6db5d038600adeec14863f2076a4be96
Ahmed Radwan <aradwann@proton.me>
2025-02-02 19:06:14 +0200

    add subscriptions endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{dev::Server, get, post, web, App, HttpResponse, HttpServer};
 
 #[get("/health-check")]
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+#[post("/subscriptions")]
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().service(health_check))
+    let server = HttpServer::new(|| App::new().service(health_check).service(subscribe))
         .listen(listener)?
         .run();
     Ok(server)
