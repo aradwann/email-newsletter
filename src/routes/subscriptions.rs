0faf0a6e84dca93922998d9797cdf65861e16918
Ahmed Radwan <aradwann@proton.me>
2025-02-15 01:50:27 +0200

    refactor parsing form to subscriber into using type conversion trait

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -23,20 +23,24 @@ struct FormData {
     fields(email = %form.email, name = %form.name)
 )]
 pub async fn subscribe(form: Form<FormData>, pool: Data<PgPool>) -> HttpResponse {
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
+    let new_subscriber = match form.0.try_into() {
+        Ok(subscriber) => subscriber,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-
-    let email = match SubscriberEmail::parse(form.0.email) {
-        Ok(email) => email,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-    let new_subscriber = NewSubscriber { email, name };
 
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name.clone()).map_err(|e| e.to_string())?;
+        let email = SubscriberEmail::parse(value.email.clone()).map_err(|e| e.to_string())?;
+        Ok(NewSubscriber { email, name })
     }
 }
 
